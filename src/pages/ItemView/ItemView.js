debdcd2ae1be2f79f77a4ab43ede0aa8995218f6
AndrewChacon <andrew.b.chacon@gmail.com>
2020-02-10 02:42:57 -0500

    Item View styles and media queries

--- src/pages/ItemView/ItemView.js
+++ src/pages/ItemView/ItemView.js
@@ -5,28 +5,27 @@ import ItemImage from "../../img/image.png";
 
 export const ItemView = () => {
   return (
-    <Card>
-      <div className="item-view">
+    <Card className="item-view">
         <div>
-          <img className="item-view__image" src={ItemImage} name="Item Image" />
+          <img className="item-view__image" src={ItemImage} alt="Product" />
         </div>
-        <div>
-          <h1>Product Name</h1>
-          <h4>Product Price</h4>
-          <Alert color="secondary">
+        <div className="item-view__cart-details">
+          <h1 className="item-view__product-name">Product Name</h1>
+          <h4 className="item-view__product-price">Product Price</h4>
+          <Alert color="secondary" className="item-view__product-id">
             Product ID Code
           </Alert>
           <Form inline>
-            <Input type="number" name="productNumber" />
-            <Button>Add To Cart</Button>
+            <Input className="item-view__input" type="number" name="productNumber" />
+            <Button className="item-view__form-button">Add To Cart</Button>
           </Form>
         </div>
-        <div>
-          <h3>About This Product</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <Button color="danger">Delete Product</Button>
+        <div className="item-view__product-details">
+          <h3 className="item-view__product-heading">About This Product</h3>
+          <p className="item-view__product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <Button className="item-view__delete-button" color="danger">Delete Product</Button>
         </div>
-      </div>
+
     </Card>
   )
 }
