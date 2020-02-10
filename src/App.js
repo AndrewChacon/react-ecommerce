85d6a11e46570aabe8bf52a473f1f982d006ba02
AndrewChacon <andrew.b.chacon@gmail.com>
2020-02-10 07:27:01 -0500

    Item View page and responsive styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from './components/layout/Navigation/Navigation';
-// import { Shopping } from './pages/Shopping/Shopping';
+import { Shopping } from './pages/Shopping/Shopping';
 import { ItemView } from './pages/ItemView/ItemView';
 import { Container } from 'reactstrap';
 
--- src/pages/ItemView/ItemView.js
+++ src/pages/ItemView/ItemView.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./ItemView.scss";
 import { Card, Button, Input, Form, Alert } from 'reactstrap';
 import ItemImage from "../../img/image.png";
 
 export const ItemView = () => {
+  const [productNumber, setProductNumber] = useState(1);
   return (
     <Card className="item-view">
-        <div>
+        <div className="item-view__product-display">
           <img className="item-view__image" src={ItemImage} alt="Product" />
         </div>
         <div className="item-view__cart-details">
-          <h1 className="item-view__product-name">Product Name</h1>
-          <h4 className="item-view__product-price">Product Price</h4>
+          <h1 className="item-view__product-name">Pewdiepie's Gaming Chair</h1>
+          <h4 className="item-view__product-price">$399.99</h4>
           <Alert color="secondary" className="item-view__product-id">
-            Product ID Code
+            SKU: 726-81-7094
           </Alert>
-          <Form inline>
-            <Input className="item-view__input" type="number" name="productNumber" min="1" max="99" />
-            <Button className="item-view__form-button">Add To Cart</Button>
+          <Form>
+            <div className="item-view__form-group">
+              <Input className="item-view__input"
+                type="number" name="productNumber" min="1" max="99"
+                value={productNumber}
+                onChange={e => setProductNumber(e.target.value)}
+              />
+              <Button className="item-view__form-button">Add To Cart &nbsp;<i className="fas fa-plus"></i></Button>
+            </div>
           </Form>
         </div>
         <div className="item-view__product-details">
           <h3 className="item-view__product-heading">About This Product</h3>
-          <p className="item-view__product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <Button className="item-view__delete-button" color="danger">Delete Product</Button>
+          <p className="item-view__product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed. Volutpat consequat mauris nunc congue nisi vitae. Fames ac turpis egestas sed tempus urna et pharetra. Auctor urna nunc id cursus metus aliquam eleifend mi. Id donec ultrices tincidunt arcu non sodales. Aliquet nibh praesent tristique magna sit amet purus gravida quis. Eu facilisis sed odio morbi quis commodo odio aenean sed. </p>
+          <Button className="item-view__delete-button" color="danger">Delete Product &nbsp;<i className="fas fa-trash-alt"></i></Button>
         </div>
-
     </Card>
   )
 }
