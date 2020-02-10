527ce1e8cd06749fed551801e7ab7c1763ef4098
AndrewChacon <andrew.b.chacon@gmail.com>
2020-02-10 07:35:45 -0500

    button font-size error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from './components/layout/Navigation/Navigation';
 import { Shopping } from './pages/Shopping/Shopping';
 import { ItemView } from './pages/ItemView/ItemView';
 import { Container } from 'reactstrap';
+import { CreateItem } from './pages/CreateItem/CreateItem';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
        <Container>
         <ItemView />
         {/* <Shopping /> */}
+        {/* <CreateItem /> */}
        </Container>
     </div>
   );
---
+++ src/pages/CreateItem/CreateItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CreateItem = () => {
+  return (
+    <div>
+      <h1>Create item</h1>
+    </div>
+  )
+}
