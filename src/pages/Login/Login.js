c8deff49e1bb6dd07336305ac066256f92a7740e
AndrewChacon <andrew.b.chacon@gmail.com>
2020-02-13 16:02:50 -0500

    login and register form state/ logic

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import "./Login.scss"
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
       <h1>Login</h1>
       <Form>
       <FormGroup>
         <Label>Email</Label>
-        <Input type="email" />
+        <Input type="email" value={email} onChange={e => setEmail(e.target.value)} />
       </FormGroup>
       <FormGroup>
         <Label>Password</Label>
-        <Input type="password" />
+        <Input type="password"  value={password} onChange={e => setPassword(e.target.value)} />
       </FormGroup>
       <Button>Login</Button>
     </Form>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import "./Register.scss"
 
 export const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
   return (
     <div>
       <h1>Register</h1>
       <Form>
       <FormGroup>
         <Label>Name</Label>
-        <Input type="text" />
+        <Input type="text" value={name} onChange={e => setName(e.target.value)} />
       </FormGroup>
       <FormGroup>
         <Label>Email</Label>
-        <Input type="email" />
+        <Input type="email" value={email} onChange={e => setEmail(e.target.value)} />
       </FormGroup>
       <FormGroup>
         <Label>Password</Label>
-        <Input type="password" />
+        <Input type="password" value={password} onChange={e => setPassword(e.target.value)} />
       </FormGroup>
       <FormGroup>
         <Label>Confirm Password</Label>
-        <Input type="password" />
+        <Input type="password" value={password2} onChange={e => setPassword2(e.target.value)} />
       </FormGroup>
       <Button>Submit</Button>
     </Form>
