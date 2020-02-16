6fab416ee0f54176cc46099d74eda86d7e1b06e8
AndrewChacon <andrew.b.chacon@gmail.com>
2020-02-15 19:16:23 -0500

    navigation bar authentication logic

--- src/components/layout/Navigation/Navigation.js
+++ src/components/layout/Navigation/Navigation.js
@@ -1,21 +1,14 @@
-import React, { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarText
-} from 'reactstrap';
+import React, { useState, Fragment } from 'react';
+import { Collapse, Navbar,  NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import "./Navigation.scss";
 import { Link } from "react-router-dom";
 
 const Navigation = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   const toggle = () => setIsOpen(!isOpen);
+  const toggleNavigation = () => setIsAuthenticated(!isAuthenticated);
 
   return (
 
@@ -32,33 +25,47 @@ const Navigation = (props) => {
                 <Link className="navigation-bar__link" to="/cart">Cart &nbsp;<i className="fas fa-shopping-cart"></i></Link>
               </NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink>
-                <Link className="navigation-bar__link" to="/create">Create &nbsp;<i className="far fa-plus-square"></i></Link>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                <Link className="navigation-bar__link" to="/account">Account &nbsp;<i className="fas fa-user"></i></Link>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                <Link className="navigation-bar__link" to="/login">Login &nbsp;<i class="far fa-user-circle"></i></Link>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                <Link className="navigation-bar__link" to="/register">Register &nbsp;<i class="far fa-user-circle"></i></Link>
-              </NavLink>
-            </NavItem>
-            {/* <NavItem>
-              <NavLink href="/">Log Out &nbsp;<i className="fas fa-sign-out-alt"></i></NavLink>
-            </NavItem> */}
+            {
+              isAuthenticated ? 
+              (
+                <Fragment>
+                  <NavItem>
+                    <NavLink>
+                      <Link className="navigation-bar__link" to="/create">Create &nbsp;<i className="far fa-plus-square"></i></Link>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink>
+                      <Link className="navigation-bar__link" to="/create">Orders &nbsp;<i className="far fa-plus-square"></i></Link>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink>
+                      <Link className="navigation-bar__link" to="/account">Account &nbsp;<i className="fas fa-user"></i></Link>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink>
+                      <Link onClick={toggleNavigation} className="navigation-bar__link">Log Out &nbsp;<i className="fas fa-user"></i></Link>
+                    </NavLink>
+                  </NavItem>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <NavItem>
+                    <NavLink>
+                      <Link className="navigation-bar__link" to="/login">Login &nbsp;<i class="far fa-user-circle"></i></Link>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink>
+                      <Link className="navigation-bar__link" to="/register">Register &nbsp;<i class="far fa-user-circle"></i></Link>
+                    </NavLink>
+                  </NavItem>
+                </Fragment>
+              )
+            }
           </Nav>
-          {/* <NavbarText>
-          <Link className="navigation-bar__link" to="/register">Register &nbsp;<i class="far fa-user-circle"></i></Link>
-          </NavbarText> */}
         </Collapse>
       </Navbar>
 
