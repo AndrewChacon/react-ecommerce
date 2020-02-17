2d3372ca147ee8b07abccd2615ce80c1f743f42f
AndrewChacon <andrew.b.chacon@gmail.com>
2020-02-17 14:49:02 -0500

    orders state/logic

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Orders.scss"
 import { Button, Form, FormGroup, Label, Input, Table } from 'reactstrap'
 
 export const Orders = () => {
+  const [orders, setOrders] = useState([
+    {
+      id: 1,
+      orderNumber: 319702,
+      purchaseDate: Date.now(),
+      total: 99.97
+    },
+    {
+      id: 2,
+      orderNumber: 31873,
+      purchaseDate: Date.now(),
+      total: 20.51
+    },
+    {
+      id: 3,
+      orderNumber: 319718,
+      purchaseDate: Date.now(),
+      total: 420.69
+    }
+  ])
   return (
     <div className="orders">
       <h1>Orders</h1>
@@ -15,7 +35,20 @@ export const Orders = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          {
+            orders.length > 0 ?
+            (orders.map(order => {
+              const { orderNumber, purchaseDate, total } = order;
+            return <tr><th scope="row">{orderNumber}</th><td>{purchaseDate}</td><td>${total}</td></tr>})
+            ) : (<h1>You have no previous orders</h1>)
+          }
+        </tbody>
+      </Table>
+    </div>
+  )
+}
+
+          {/* <tr>
             <th scope="row">113-7112181-7070633</th>
             <td>2/16/2020</td>
             <td>$74.40</td>
@@ -24,9 +57,4 @@ export const Orders = () => {
             <th scope="row">973-5212027-2280628</th>
             <td>2/17/2020</td>
             <td>$99.97</td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  )
-}
+          </tr> */}
